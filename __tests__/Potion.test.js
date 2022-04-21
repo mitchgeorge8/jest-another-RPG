f84a61e862e9e28217a00e3c5fa6607aba3d2b54
Mitchell George <mitchgeorge8@gmail.com>
2022-04-21 19:46:26 -0400

    10.1.7 Refactor the code to handle random potion creation

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -6,3 +6,11 @@ test("Creates a health potion object", () => {
   expect(potion.name).toBe("health");
   expect(potion.value).toEqual(expect.any(Number));
 });
+
+test("Creates a random potion object", () => {
+  const potion = new Potion();
+
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
+});
